96906a94f2027db77827cf1a5f2894d8e0658acc
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-06-06 21:20:26 +0000

    Done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit."
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
